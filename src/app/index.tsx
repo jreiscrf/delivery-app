1974838765aaa0768319ddfb108b977764ad0086
jreiscrf <jonathan.reis.torres@gmail.com>
2024-02-06 17:20:59 -0300

    feat: componentes de detalhes do produto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { View, FlatList, SectionList, Text } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { Link } from "expo-router";
+
 import { CategoryButton } from "@/components/category-button";
+import { Product } from "@/components/product";
 import { Header } from "@/components/header";
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from "@/utils/data/products";
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0] || "");
 
+  const sectionListRef = useRef<SectionList>(null);
+
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory);
+
+    const sectionIndex = CATEGORIES.findIndex(
+      (item) => item === selectedCategory
+    );
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      });
+    }
   }
 
   return (
@@ -29,6 +46,24 @@ export default function Home() {
         className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+
+      <SectionList
+        sections={MENU}
+        ref={sectionListRef}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-white font-heading mt-8 mb-3">{title}</Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   );
