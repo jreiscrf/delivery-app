7e850048da13d53cb0550e1b4f56f8b2e584c3ee
jreiscrf <jonathan.reis.torres@gmail.com>
2024-02-10 13:39:04 -0300

    feat: routes

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,7 @@
 import { View, Image, Text } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Feather } from "@expo/vector-icons";
+import { Redirect } from "expo-router";
 
 import { Button } from "@/components/button";
 import { PRODUCTS } from "@/utils/data/products";
@@ -13,11 +14,15 @@ export default function Product() {
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddToCart() {
-    cartStore.add(product);
+    cartStore.add(product!); // Validando se o produto existe com a ! (Não será undefined)
     navigation.goBack();
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -29,6 +34,10 @@ export default function Product() {
       />
 
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading ">
+          {product.title}
+        </Text>
+
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
